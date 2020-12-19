19702a38de699c7f5459bd8ca35eb6c4f5de48a0
amnhh <2570079254@qq.com>
2020-12-19 12:11:50 +0800

    chore: add eslint support

--- utils/generator/Array.ts
+++ utils/generator/Array.ts
@@ -4,26 +4,26 @@ const { disorder, unique, duplicate } = ArrayLang
 /**
  * 初始化空数组
  */
-export const genEmptyArr = (size: number = 10): null[] => Array(size).fill(null)
+export const genEmptyArr = (size = 10): null[] => Array(size).fill(null)
 
 /**
  * 初始化有序数组（由下标填充）
  */
-export const genSortedArrWithIdx = (size: number = 10): number[] => genEmptyArr(size).map((v, i) => i)
+export const genSortedArrWithIdx = (size = 10): number[] => genEmptyArr(size).map((v, i) => i)
 
 /**
  * 初始化无序数组
  */
-export const genUnsortedFilledArr = (size: number = 10): number[] => genSortedArrWithIdx(size).map(v => Math.floor(v + Math.random() * size))
+export const genUnsortedFilledArr = (size = 10): number[] => genSortedArrWithIdx(size).map(v => Math.floor(v + Math.random() * size))
 
 /**
  * 带有重复元素的乱序数组
  * 初始化 -> 添加重复元素 -> 乱序
  */
-export const genDuplicateUnsortedArr = (size: number = 10): number[] => disorder(duplicate(genSortedArrWithIdx(size)))
+export const genDuplicateUnsortedArr = (size = 10): number[] => disorder(duplicate(genSortedArrWithIdx(size)))
 
 /**
  * 不带重复元素的乱序数组
  * 初始化 -> 唯一化 -> 乱序
  */
-export const genUnduplicateUnsortedArray = (size: number = 10): number[] => disorder(unique(genUnsortedFilledArr(size)))
+export const genUnduplicateUnsortedArray = (size = 10): number[] => disorder(unique(genUnsortedFilledArr(size)))
